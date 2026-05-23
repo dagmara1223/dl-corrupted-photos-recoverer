cc39fa7c7152d9633b97b18a9ea3ccb8d1d02098
nkucht4 <n4t523@gmail.com>
2026-05-23 21:08:21 +0200

    Add: hopefully working model inference

---
+++ disk_scanner/src/model.rs
@@ -0,0 +1,30 @@
+use ort::session::Session;
+use ort::value::Value;
+use ndarray::Array;
+
+pub struct Model {
+    session: Session,
+}
+
+impl Model {
+    pub fn new(model_path: &str) -> Self {
+        let session = Session::builder()
+            .unwrap()
+            .commit_from_file(model_path)
+            .unwrap();
+
+        Self { session }
+    }
+
+    pub fn predict(&mut self, input: Vec<f32>) -> f32 {
+        let input = Array::from_shape_vec((1, 3, 256, 256), input).unwrap();
+
+        let input = Value::from_array(input).unwrap();
+
+        let outputs = self.session.run(vec![("input", input),]).unwrap();
+
+        let out = outputs[0].try_extract_tensor::<f32>().unwrap();
+
+        out.1[0]
+    }
+}
